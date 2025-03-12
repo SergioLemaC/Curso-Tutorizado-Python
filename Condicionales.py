30f21a8d6d5905c6d261bb92817a009f2fe0e8a8
lemac1nside <slemac@unicartagena.edu.co>
2025-03-12 14:42:52 -0500

    Terminando condicionales

--- Condicionales.py
+++ Condicionales.py
@@ -37,3 +37,40 @@ if (0<edad<100):
 
 else:
     print("Edad incorrecta")
+
+print("Programa de becas año 2017")
+
+distancia_escuela = int(input("Introduce la distancia a la escuela en km "))
+
+print(distancia_escuela)
+
+numero_hermanos = int(input("Introduce el número de hermanos en el centro "))
+
+print(numero_hermanos)
+
+salario_familiar = int(input("Introduce salario anual bruto "))
+
+print(salario_familiar)
+
+if(distancia_escuela > 40 and numero_hermanos > 2 or salario_familiar <= 20000):
+    print("Tienes derecho a beca")
+
+else:
+    print("No tienes derecho a beca")
+
+
+#Escoger asignatura optativa
+
+print("Asignaturas optativas")
+
+print("Informática gráfica - Pruebas de Software - Usabilidad y accesibilidad")
+
+opcion = input("Escribe la asignatura escogida: ")
+
+asignatura = opcion.lower()
+
+if(asignatura in ("informática gráfica", "pruebas de software", "usabilidad y accesibilidad")):
+    print("Asignatura elegida " + asignatura)
+
+else:
+    print("La asignatura escogida no está contemplada")
