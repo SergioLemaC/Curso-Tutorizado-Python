34e80b0de399f4c2bd845638d591876c06940353
lemac1nside <slemac@unicartagena.edu.co>
2025-03-13 10:20:27 -0500

    Uso del in range

--- BucleFor.py
+++ BucleFor.py
@@ -15,3 +15,25 @@ if(contador == 2):
 
 else:
     print("El email no es correcto")
+
+for i in range(5):
+    print("Hola")
+    print(f"Valor de la variable {i}")
+
+#for i in range(5, 10): Debe contar desde el 5 hasta el 9
+#for i in range(5, 50, 3): Debe contar desde el 5 hasta el 49 de 3 en 3
+
+valido = False
+
+email = input("Introduce tu email: ")
+
+for i in range(len(email)):
+
+    if(email[i] == "@"):
+        valido = True
+
+if (valido):
+    print("Email correcto")
+
+else:
+    print("Email incorrecto")
